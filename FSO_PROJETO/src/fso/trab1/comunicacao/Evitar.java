cd03c8e659d6ba20786d69fad591e084f5fe4a93
w4terboy <alfifer1@gmail.com>
2022-11-22 15:20:52 +0000

    aaa

--- FSO_PROJETO/src/fso/trab1/comunicacao/Evitar.java
+++ FSO_PROJETO/src/fso/trab1/comunicacao/Evitar.java
@@ -1,13 +1,10 @@
 package fso.trab1.comunicacao;
 
-import java.util.Random;
-
 import javax.swing.JTextArea;
 
 import robot.RobotLegoEV3;
 
 public class Evitar extends Administrador{
-	private Random rnd;
 	JTextArea textAreaConsola;
 	private RobotLegoEV3 r;
 	
