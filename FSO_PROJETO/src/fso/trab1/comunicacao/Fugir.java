c0245f755d6dfcccb990f79ca450c8c10cb6cc5d
w4terboy <alfifer1@gmail.com>
2022-11-22 15:59:32 +0000

    fugir

--- FSO_PROJETO/src/fso/trab1/comunicacao/Fugir.java
+++ FSO_PROJETO/src/fso/trab1/comunicacao/Fugir.java
@@ -1,5 +1,49 @@
 package fso.trab1.comunicacao;
 
+import java.util.Random;
+
+import javax.swing.JTextArea;
+
+import robot.RobotLegoEV3;
+
 public class Fugir extends Administrador{
+	JTextArea textAreaConsola;
+	private Random rnd;
+	private RobotLegoEV3 r;
+	
+	public void fugir() {
+		try {
+			Thread.sleep(250);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int rand_int = rnd.nextInt(2);
+		
+		if(r.SensorUS(RobotLegoEV3.S_2) <= 50) {
+			for (int i = 0; i < 6; i++) {
+				
+				switch(i) {
+				case 0:
+					r.Reta(50);;
+					break;
+				case 1:
+					if(rand_int == 1) {
+						r.CurvarDireita(0, 90);
+					}else {
+						r.CurvarEsquerda(0, 90);
+					}
+					break;
+				case 2:
+					r.CurvarEsquerda(0, 90);
+					break;
+				case 3:
+					r.Parar(true);
+					break;
+				}
+			}
+		}
+	}
 
 }
